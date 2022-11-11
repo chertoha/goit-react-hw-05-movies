b6a8e21d6c07deea30890ac50090d7b7be90cdf7
Anton <a.chertok@akatech.com.ua>
2022-11-11 11:47:26 +0200

    routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import {Routes, Route, Link, Outlet} from 'react-router-dom'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-     Ant - Che 
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<div>HEADER <Link to="/">Home</Link> <Link to="/movies">Movies</Link> <Outlet/> </div> }>
+        <Route index element={<div>Trending Today <Link to="/movies/1313">movie link</Link></div>} />
+        <Route path="movies" element={<div>Movies</div>} />
+        <Route path="movies/:movieId" element={<div>Movies/:movieId <br/> Aditional info <Link to="cast">Cast</Link> <Link to="reviews">Reviews</Link> <Outlet/> </div>} >
+          <Route path="cast" element={ <div>Cast</div>} />
+          <Route path="reviews" element={ <div>Reviews</div>} />
+        </Route>
+        
+      </Route>
+    </Routes>
+  )
+    
+  
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+import { App } from 'components/App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'utils/theme';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <BrowserRouter basename="/goit-react-hw-05-movies/"> */}
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
