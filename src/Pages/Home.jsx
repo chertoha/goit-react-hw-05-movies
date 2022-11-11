4a337385dfd9de508681f191290fb115f69383b5
Anton <a.chertok@akatech.com.ua>
2022-11-11 20:41:03 +0200

    Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,27 @@
+import Box from 'components/Box';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getTrendings} from 'services/movieDatabaseApi';
+
 const Home = () => {
+    const [movies, setMovies] = useState([]);
     
-    return (<div>Trending Today <Link to="/movies/1313">movie link</Link></div>);
+    useEffect(() => {
+        getTrendings().then(setMovies).catch(console.log);
+    },[])
+
+    return (
+        <Box as="main">
+            <h1>Tranding today</h1>
+            <ul>
+                {movies.map(movie => {
+                    return (<li key={ movie.id}><Link>{ movie.title}</Link></li>)
+                    
+                })}
+            </ul>
+        </Box>
+        // <div>Trendingd Today <Link to="/movies/1313">movie link</Link></div>
+    );
 }
+
+export default Home;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route, Link, Outlet } from 'react-router-dom';
 import Layout from 'components/Layout';
+import Home from 'pages/Home';
 
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout/> }>
-        <Route index element={<div>Trending Today <Link to="/movies/1313">movie link</Link></div>} />
+        <Route index element={<Home/>} />
         <Route path="movies" element={<div>Movies</div>} />
         <Route path="movies/:movieId" element={<div>Movies/:movieId <br/> Aditional info <Link to="cast">Cast</Link> <Link to="reviews">Reviews</Link> <Outlet/> </div>} >
           <Route path="cast" element={ <div>Cast</div>} />
