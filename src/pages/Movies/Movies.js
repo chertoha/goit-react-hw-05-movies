a145d6d7df91c86caeb843d2d5ce785b82ee13fd
Anton <a.chertok@akatech.com.ua>
2022-11-13 21:47:14 +0200

    Update Movies.js

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -2,74 +2,53 @@ import MoviesList from 'components/MoviesList';
 import { useEffect, useState } from 'react';
 import { useLocation, useSearchParams } from 'react-router-dom';
 import { searchMovie } from 'services/movieDatabaseApi';
-import { STATUS } from 'utils/config';
 
 const Movies = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const location = useLocation();
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState(null);
   const [error, setError] = useState(null);
-  //   const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const query = searchParams.get('query');
 
   useEffect(() => {
     if (!query) return;
 
+    setLoading(true);
     searchMovie(query)
       .then(result => {
-        // console.log(result);
+        console.log(result);
         setMovies(result);
       })
       .catch(error => {
         setError(error.message);
-      });
+      })
+      .finally(setLoading(false));
   }, [query]);
 
   const onSearch = e => {
     e.preventDefault();
-    // setStatus(STATUS.PENDING);
     const value = e.target.elements.search.value;
-
     e.target.reset();
     setSearchParams(value === '' ? {} : { query: value });
-    // setSearch(search);
-    // searchMovie(query)
-    //   .then(result => {
-    //     // console.log(result);
-    //     setStatus(STATUS.RESOLVED);
-    //     setMovies(result);
-    //   })
-    //   .catch(error => {
-    //     setStatus(STATUS.REJECTED);
-    //     setError(error.message);
-    //   });
   };
 
-  //   const onInputChange = e => {
-  //     const value = e.target.value;
-  //     setSearchParams(value === '' ? {} : { query: value });
-  //   };
-
-  console.log(movies);
   return (
     <>
       <form onSubmit={onSearch}>
-        <input
-          type="text"
-          name="search"
-          placeholder="Search movie"
-          //   onChange={onInputChange}
-          //   value={query ?? ''}
-        />
+        <input type="text" name="search" placeholder="Search movie" />
         <button type="submit">Search</button>
       </form>
 
-      <MoviesList
-        movies={movies}
-        path={'/movies/'}
-        state={{ from: location }}
-      />
+      {loading && <div>Loading...</div>}
+      {movies ?? (
+        <MoviesList
+          movies={movies}
+          path={'/movies/'}
+          state={{ from: location }}
+        />
+      )}
     </>
   );
 };
